e0f1b25a695f81455ffb8bdc67cc1bed83e5ce50
Rex Tsai <chihchun@kalug.linux.org.tw>
2010-12-03 15:54:23 +0800

    added open the project, update database

--- visualizer.py
+++ visualizer.py
@@ -18,58 +18,62 @@
 
 import sys
 import os
+import subprocess
 
+import gobject
 import gtk
 import gtk.gdk
 
 # On debian, xdot isn't installed in the system namespace:
 sys.path.insert(1, '/usr/share/xdot')
 import xdot
-import gobject
-import gtk
-import gtk.gdk
 
+class Visualizer(xdot.DotWindow):
 
-class MyDotWindow(xdot.DotWindow):
-
-    ui = '''
-    <ui>
-        <toolbar name="ToolBar">
-            <toolitem action="Open"/>
-            <separator/>
-            <toolitem action="ZoomIn"/>
-            <toolitem action="ZoomOut"/>
-            <toolitem action="ZoomFit"/>
-            <toolitem action="Zoom100"/>
-        </toolbar>
-    </ui>
-    '''
-
+    interest = {}
+    working_dir = None
 
     def __init__(self):
         xdot.DotWindow.__init__(self)
-        self.widget.connect('clicked', self.on_url_clicked)
-        toolbar = self.uimanager.get_widget('/ToolBar')
-        vbox = self.get_child()
 
+        """
+        self.actiongroup.add_actions((
+            ('New',  gtk.STOCK_OPEN, None, None, None, self.on_open),
+            ));
+        """
+        self.widget.connect('clicked', self.on_url_clicked)
+
+        toolbar = self.uimanager.get_widget('/ToolBar')
+
+        item = gtk.ToolButton(gtk.STOCK_SAVE)
+        item.set_tooltip_markup("Save")
+        item.connect('clicked', self.on_save)
+        item.show()
+        toolbar.insert(item, 0)
+
+        item = gtk.ToolButton(gtk.STOCK_NEW)
+        item.set_tooltip_markup("New project")
+        item.connect('clicked', self.on_newproject)
+        item.show()
+        toolbar.insert(item, 0)
+        
+        vbox = self.get_child()
         hbox = gtk.HBox()
 
-        label = gtk.Label("Search symbols: ");
+        label = gtk.Label("Search symbol: ");
         hbox.pack_start(label, False)
         label.show()
 
         entry = gtk.Entry(max=0)
-        # entry.connect('enter-notify-event', self.on_symbol_enter)
-        # entry.connect('key_press', self.on_symbol_enter)
         entry.connect('activate', self.on_symbol_enter)
         hbox.pack_start(entry, True, True, 10)
         entry.show()
-
 
         vbox.pack_start(hbox, False)
         vbox.reorder_child(hbox, 1)
         hbox.show()
 
+        """ code brower, maybe someday I will implement it """
 #        text = gtk.TextBuffer()
 #        text.set_text("Hello world")
 #        viewer = gtk.TextView(text)
@@ -77,45 +81,143 @@ class MyDotWindow(xdot.DotWindow):
 #        viewer.show()
 
     def on_symbol_enter(self, widget):
-        print widget.get_text()
+        symbol = widget.get_text()
         widget.set_text('')
 
+        if self.working_dir is None:
+            # FIXME: let's have a dialog for the user.
+            self.on_newproject(None)
+
+        # TODO: Saving the filename and line number.
+        self.interest[symbol] = 1
+        self.update_graph()
+
+    def update_graph(self):
+        """ update dot code based on the interested keys """
+        funcs = set(self.interest.keys())
+        dotcode = "digraph G {"
+        for func in funcs:
+            dotcode += "\"%s\"" % func
+            allFuncs, funsCalled = self.functionsCalled (func)
+            for m in (allFuncs & funcs):
+                dotcode += "\"%s\" -> \"%s\"" % (func, m)
+        dotcode += "}"
+        self.set_dotcode(dotcode)
+
     def on_url_clicked(self, widget, url, event):
-        dialog = gtk.MessageDialog(
-                parent = self, 
-                buttons = gtk.BUTTONS_OK,
-                message_format="%s clicked" % url)
+        dialog = gtk.MessageDialog(parent = self, buttons = gtk.BUTTONS_OK, message_format="%s clicked" % url)
         dialog.connect('response', lambda dialog, response: dialog.destroy())
         dialog.run()
         return True
 
-    def set_dotcode(self, dotcode, filename='<stdin>'):
+    def set_dotcode(self, dotcode, filename=''):
         if self.widget.set_dotcode(dotcode, filename):
             self.set_title(os.path.basename(filename) + ' - Code Visualizer')
             self.widget.zoom_to_fit()
 
-    def set_xdotcode(self, xdotcode, filename='<stdin>'):
+    def set_xdotcode(self, xdotcode, filename=''):
         if self.widget.set_xdotcode(xdotcode):
             self.set_title(os.path.basename(filename) + ' - Code Visualizer')
             self.widget.zoom_to_fit()
 
+    def update_database(self):
+        if not os.path.isfile(self.working_dir + "/cscope.out"):
+            dialog = gtk.MessageDialog(parent=self, type=gtk.MESSAGE_QUESTION, buttons=gtk.BUTTONS_YES_NO)
+            dialog.set_default_response(gtk.RESPONSE_YES)
+            dialog.set_markup("Create cscope database for %s now ?" % filename)
+            ret = dialog.run()
+            dialog.destroy()
+            if ret == gtk.RESPONSE_YES:
+                cmd = "cscope -bkRu"
+                process = subprocess.call(cmd, shell = True, cwd = self.working_dir) 
+                del process
+        pass
 
-dotcode = """
-digraph G {
-  Hello [UR2="http://en.wikipedia.org/wiki/Hello"]
-  World [URL="http://en.wikipedia.org/wiki/World"]
-    Hello -> World
-}
-"""
+    def cscope(self, mode, func):
+        cmd = "cscope -d -l -L -%d %s" % (mode, func) 
+        process = subprocess.Popen(cmd, stdout = subprocess.PIPE, shell = True, cwd = self.working_dir) 
+        csoutput = process.stdout.read() 
+        del process
+        cslines = [arr.strip().split(' ') for arr in csoutput.split('\n') if len(arr.split(' '))>1] 
+        allFuns = set(map(lambda x:x[1], cslines))
 
+        funsCalled = {}
+        for fl in cslines:
+            if funsCalled.has_key(fl[0]):
+                funsCalled[fl[0]] |= set([fl[1]])
+            else:
+                funsCalled[fl[0]] = set([fl[1]])
+
+        return (allFuns, funsCalled)
+
+    def functionDefincation(self, func):
+        return self.cscope(1, entryFun)
+
+    def functionsCalled(self, entryFun):
+        # Find functions called by this function:
+        return self.cscope(2, entryFun)
+
+    def on_open(self, action):
+        chooser = gtk.FileChooserDialog(title="Open dot File",
+                                        action=gtk.FILE_CHOOSER_ACTION_OPEN,
+                                        buttons=(gtk.STOCK_CANCEL,
+                                                 gtk.RESPONSE_CANCEL,
+                                                 gtk.STOCK_OPEN,
+                                                 gtk.RESPONSE_OK))
+        chooser.set_default_response(gtk.RESPONSE_OK)
+        filter = gtk.FileFilter()
+        filter.set_name("Graphviz dot files")
+        filter.add_pattern("*.dot")
+        chooser.add_filter(filter)
+        filter = gtk.FileFilter()
+        filter.set_name("All files")
+        filter.add_pattern("*")
+        chooser.add_filter(filter)
+        if chooser.run() == gtk.RESPONSE_OK:
+            filename = chooser.get_filename()
+            chooser.destroy()
+            self.open_file(filename)
+        else:
+            chooser.destroy()
+
+    def on_save(self, action):
+        # gtk.FILE_CHOOSER_ACTION_SAVE
+        pass
+
+    def on_newproject(self, action):
+        chooser = gtk.FileChooserDialog(title="Open the source code directory",
+                                        action=gtk.FILE_CHOOSER_ACTION_SELECT_FOLDER,
+                                        buttons=(gtk.STOCK_CANCEL,
+                                                 gtk.RESPONSE_CANCEL,
+                                                 gtk.STOCK_OPEN,
+                                                 gtk.RESPONSE_OK))
+        chooser.set_default_response(gtk.RESPONSE_OK)
+        if chooser.run() == gtk.RESPONSE_OK:
+            filename = chooser.get_filename()
+            chooser.destroy()
+            self.working_dir = filename
+            self.update_database()
+        else:
+            chooser.destroy()
+        pass
+
+    def open_file(self, filename):
+        try:
+            fp = file(filename, 'rt')
+            self.set_dotcode(fp.read(), filename)
+            fp.close()
+        except IOError, ex:
+            dlg = gtk.MessageDialog(type=gtk.MESSAGE_ERROR,
+                                    message_format=str(ex),
+                                    buttons=gtk.BUTTONS_OK)
+            dlg.set_title('Dot Viewer')
+            dlg.run()
+            dlg.destroy()
 
 def main():
-    window = MyDotWindow()
-    window.set_dotcode(dotcode)
-    window.set_dotcode(dotcode)
+    window = Visualizer()
     window.connect('destroy', gtk.main_quit)
     gtk.main()
-
 
 if __name__ == '__main__':
     main()
